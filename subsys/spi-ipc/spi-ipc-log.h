ad64f40bca3ab427541a0e78ce936717aee5c774
Davide Ciminaghi <ciminaghi@gnudd.com>
2019-05-29 15:30:20 +0200

    subsys: add first draft of spi-ipc core implementation
    See
    
    https://github.com/linino/zephyr/wiki/esp8266-as-a-zephyr-network-interface
    
    for more details
    
    Signed-off-by: Davide Ciminaghi <ciminaghi@gnudd.com>

---
+++ subsys/spi-ipc/spi-ipc-log.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2019 dog hunter LLC and the Linino community
+ * Generic inter-processor communication via spi link, log related stuff
+ *
+ * Author Davide Ciminaghi <davide@linino.org>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define LOG_MODULE_NAME spi_ipc_log
+#define LOG_LEVEL CONFIG_SPI_IPC_LOG_LEVEL
+
+#include <logging/log.h>
+
+#ifdef __cplusplus
+}
+#endif
