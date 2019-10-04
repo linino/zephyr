2dfca81f483e2192a1968c395b9a8586fe69945e
Davide Ciminaghi <ciminaghi@gnudd.com>
2019-10-04 17:19:44 +0200

    SPI-IPC, spi-ipc-ethernet: make net buffer pool larger
    Signed-off-by: Davide Ciminaghi <ciminaghi@gnudd.com>

--- subsys/spi-ipc/spi-ipc-ethernet.c
+++ subsys/spi-ipc/spi-ipc-ethernet.c
@@ -17,7 +17,7 @@
 LOG_MODULE_DECLARE(LOG_MODULE_NAME);
 
 /* Tx buffer pool */
-NET_BUF_POOL_DEFINE(eth_spi_ipc_pool, 64,
+NET_BUF_POOL_DEFINE(eth_spi_ipc_pool, 256,
 		    sizeof(union spi_thb),
 		    sizeof(struct spi_msg *), spi_ipc_net_buf_destroy);
 
